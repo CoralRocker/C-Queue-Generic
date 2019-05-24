3a44a14d2e911b485dcaccbc9cb80a73d8214646
Gaultier Delbarre <gaultiertexas@gmail.com>
2019-05-23 21:35:58 -0500

    Make generic

--- Queue.h
+++ Queue.h
@@ -3,7 +3,7 @@
 #include <string.h>
 
 typedef struct Queue {
-	int* arr;
+	void** arr;
 	int size;
 }Queue;
 
@@ -21,28 +21,28 @@ void freeQueue(Queue *q)
 	free(q);
 }
 
-int frontQueue(Queue *q)
+void* frontQueue(Queue *q)
 {
 	if(q->size > 0)
 		return q->arr[0];
 	return 0;
 }
 
-int backQueue(Queue *q)
+void* backQueue(Queue *q)
 {
 	if(q->size > 0)
 		return q->arr[q->size-1];
 	return 0;
 }
 
-void pushQueue(Queue *q, int val)
+void pushQueue(Queue *q, void* val)
 {
-	int* tmp = malloc(sizeof(int)*q->size);
-	memcpy(tmp, q->arr, sizeof(int)*q->size);
+	void** tmp = malloc(sizeof(void*)*q->size);
+	memcpy(tmp, q->arr, sizeof(void*)*q->size);
 	free(q->arr);
 	q->size++;
-	q->arr = malloc(sizeof(int)*q->size);
-	memcpy(q->arr, tmp, sizeof(int)*(q->size-1));
+	q->arr = malloc(sizeof(void*)*q->size);
+	memcpy(q->arr, tmp, sizeof(void*)*(q->size-1));
 	free(tmp);
 	q->arr[q->size-1] = val;
 }
@@ -50,19 +50,19 @@ void pushQueue(Queue *q, int val)
 void popQueue(Queue *q)
 {
 	q->size--;
-	int* tmp = malloc(sizeof(int)*q->size);
-	memcpy(tmp, q->arr + 1, sizeof(int)*q->size);
+	void** tmp = malloc(sizeof(void*)*q->size);
+	memcpy(tmp, q->arr + 1, sizeof(void*)*q->size);
 	free(q->arr);
-	q->arr = malloc(sizeof(int)*q->size);
-	memcpy(q->arr, tmp, sizeof(int)*q->size);
+	q->arr = malloc(sizeof(void*)*q->size);
+	memcpy(q->arr, tmp, sizeof(void*)*q->size);
 	free(tmp);
 }
 
 void copyQueue(Queue *q1, Queue *q2)
 {
 	free(q2->arr);
-	q2->arr = (int*)malloc(sizeof(int)*q1->size);
-	memcpy(q2->arr, q1->arr, sizeof(int)*q1->size);
+	q2->arr = (void**)malloc(sizeof(void*)*q1->size);
+	memcpy(q2->arr, q1->arr, sizeof(void*)*q1->size);
 	q2->size = q1->size;
 
 }
